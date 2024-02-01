889e188a951949dde478ae6c6f4d8756d3c383e8
dxmar <dxmar@tamu.edu>
2024-02-01 12:04:16 -0600

    Fixed the about me page style-2 and tried to fix the lag experinced with the switch style function, so I made a whole new function. It was for nothing though, the page transition is still clunky, but I understand javascript better so that cool. Added a copy eamil funnction. This is the final version of iteration 2

--- functions.js
+++ functions.js
@@ -1,17 +1,35 @@
 
 function switchStyle(){
-    style = document.getElementById("mainStyleSheet").getAttribute("href");
+    var style1 = document.getElementById("mainStyleSheet1");
+    var style2 = document.getElementById("mainStyleSheet2");
 
-    if (style == "style-2.css") {
-        document.getElementById("mainStyleSheet").setAttribute("href", "style.css");
-        localStorage.setItem("style", "style.css");
+    if (style1.disabled) { // style1 is disabled
+        style1.disabled = false; 
+        style2.disabled = true;
+        localStorage.setItem("activeStyle", "style2");
     }
-    else {
-        document.getElementById("mainStyleSheet").setAttribute("href", "style-2.css");
-        localStorage.setItem("style", "style-2.css");
+    else if (style2.disabled) { // style2 is disabled
+        style1.disabled = true;
+        style2.disabled = false;
+        localStorage.setItem("activeStyle", "style1");
     }
 }
 
 window.onload = function(){
-    document.getElementById("mainStyleSheet").setAttribute("href", localStorage.getItem("style"));
+    var activeStyleStored = localStorage.getItem("activeStyle");
+    var style1 = document.getElementById("mainStyleSheet1");
+    var style2 = document.getElementById("mainStyleSheet2");
+    if (activeStyleStored === "style1") {
+        style1.disabled = true; 
+        style2.disabled = false;
+    }
+    else if (activeStyleStored === "style2") {
+        style1.disabled = false; 
+        style2.disabled = true; 
+    }
+}
+
+function copyFunction(text) {
+    navigator.clipboard.writeText(text);
+    alert("Copied Email to Clipboard");
 }
